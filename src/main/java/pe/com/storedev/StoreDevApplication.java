9ccdaf63e45869cb1c92f9142fef3d1fd8f2df09
jmoreva21 <jackfrancomoreno15@gmail.com>
2025-10-06 21:48:41 -0500

    initialize Spring Boot project with Maven wrapper and essential configurations

---
+++ src/main/java/pe/com/storedev/StoreDevApplication.java
@@ -0,0 +1,13 @@
+package pe.com.storedev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreDevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoreDevApplication.class, args);
+    }
+
+}
