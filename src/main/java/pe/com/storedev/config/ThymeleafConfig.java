01937e28a73326b52343025f404ec5af9d7d6fce
jmoreva21 <jackfrancomoreno15@gmail.com>
2025-10-07 22:18:10 -0500

    add Thymeleaf layout dialect support and update index.html structure

---
+++ src/main/java/pe/com/storedev/config/ThymeleafConfig.java
@@ -0,0 +1,18 @@
+package pe.com.storedev.config;
+
+import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+
+@Configuration
+public class ThymeleafConfig {
+    @Bean
+    public SpringTemplateEngine templateEngine(ITemplateResolver templateResolver) {
+        SpringTemplateEngine engine = new SpringTemplateEngine();
+        engine.setTemplateResolver(templateResolver);
+        engine.addDialect(new LayoutDialect());
+        return engine;
+    }
+}
