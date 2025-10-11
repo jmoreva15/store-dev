d2ca03035fd31dbc8a1a6870eb5d35bcd757c67c
jmoreva21 <jackfrancomoreno15@gmail.com>
2025-10-11 14:20:41 -0500

    add email parameter handling in authentication failure responses and login form

--- src/main/java/pe/com/storedev/exception/AuthenticationFailureHandler.java
+++ src/main/java/pe/com/storedev/exception/AuthenticationFailureHandler.java
@@ -16,14 +16,16 @@ public class AuthenticationFailureHandler implements org.springframework.securit
             HttpServletRequest request,
             HttpServletResponse response,
             AuthenticationException exception) throws IOException, ServletException {
+        String email = request.getParameter("username");
+
         if (exception.getCause() instanceof UserInactiveException) {
-            response.sendRedirect("/login?inactive=true");
+            response.sendRedirect("/login?inactive=true&email=" + email);
         }
         else if (exception instanceof BadCredentialsException) {
-            response.sendRedirect("/login?invalid=true");
+            response.sendRedirect("/login?invalid=true&email=" + email);
         }
         else {
-            response.sendRedirect("/login?error=true");
+            response.sendRedirect("/login?error=true&email=" + email);
         }
     }
 }
